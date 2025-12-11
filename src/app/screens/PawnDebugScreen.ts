2cb0d98080e38e53663535e234f2184e45fead90
thalys <thalys@ppd.gg>
2025-12-11 10:28:42 -0300

    feat: Introduce `ScreenBase` and `PawnDebugScreen`

---
+++ src/app/screens/PawnDebugScreen.ts
@@ -0,0 +1,29 @@
+import type { AppScreens, IAppScreen, TAssetBundleId } from '∆/navigation.types'
+import { Container } from 'pixi.js'
+import { UIPawn } from '@/screens/main/UIPawn'
+
+export class PawnDebugScreen extends Container implements IAppScreen {
+  public definition: AppScreens = 'PawnDebugScreen'
+  public label: string = 'PawnDebugScreen'
+  public static assetBundles: TAssetBundleId[] = ['main']
+  private pawn: UIPawn
+
+  constructor () {
+    super()
+
+    this.pawn = new UIPawn(2)
+    this.addChild(this.pawn)
+  }
+
+  public resize ({ screen, parent }: {
+    screen: { width: number, height: number }
+    parent: { width: number, height: number }
+  }) {
+    this.pawn.x = screen.width * 0.5
+    this.pawn.y = screen.height * 0.5
+  }
+
+  public async show () {
+    await this.pawn.show()
+  }
+}
---
+++ src/app/screens/ScreenBase.ts
@@ -0,0 +1,74 @@
+import type { AppScreens, IAppScreen, TAssetBundleId } from '∆/navigation.types'
+import type { DestroyOptions, Ticker } from 'pixi.js'
+import { engine } from '∆/engine.singleton'
+import { Container } from 'pixi.js'
+import { PopupPause } from '@/popups/popup.pause'
+
+
+export class ScreenBase extends Container implements IAppScreen {
+
+  public static assetBundles: TAssetBundleId[] = []
+  public definition: AppScreens = 'ScreenBase'
+  public override label: string = 'ScreenBase'
+  protected paused = false
+
+  constructor () {
+    super()
+  }
+
+  public prepare () {
+
+  }
+
+  public destroy (opts?: DestroyOptions) {
+    this.destroy(opts)
+  }
+
+  public reset () {
+
+  }
+
+  public onLoad (progress: number) {
+
+  }
+
+  public resize ({ screen, parent }: {
+    screen: { width: number, height: number }
+    parent: { width: number, height: number }
+  }) {
+
+  }
+
+  public async show (): Promise<void> {
+
+  }
+
+  public async hide () {
+
+  }
+
+  public async pause () {
+    this.interactiveChildren = false
+    this.paused = true
+  }
+
+  public async resume () {
+    this.interactiveChildren = true
+    this.paused = false
+  }
+
+  public blur () {
+    if (!engine().navigation.currentPopup) {
+      engine().navigation.presentPopup(PopupPause)
+    }
+  }
+
+  public focus () {
+
+  }
+
+  public update (ticker: Ticker) {
+    // eslint-disable-next-line no-useless-return
+    if (this.paused) { return }
+  }
+}
