cfd2cacb9d0949bbe3c46eb5a4d78f9eafd35f3b
thalys <thalys@ppd.gg>
2025-12-11 17:03:38 -0300

    feat: decrease probability of rolling 4 for new pawns

--- src/app/math.ts
+++ src/app/math.ts
@@ -5,5 +5,5 @@ import { rollFloat } from '∆/utils/random'
  * @returns 2 or 4
  */
 export function rollNewPawnValue (): 2 | 4 {
-  return rollFloat() >= 0.9 ? 4 : 2
+  return rollFloat() >= 0.95 ? 4 : 2
 }
