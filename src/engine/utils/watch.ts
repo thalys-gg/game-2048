470b1145994bc5897e859399b9890f4193141264
thalys <thalys@ppd.gg>
2025-12-12 14:08:36 -0300

    feat: add `watchObject` utility for reactive property observation

---
+++ src/engine/utils/watch.ts
@@ -0,0 +1,29 @@
+import type { WatchedObject, WatchedProp, WatcherCallback } from '∆/utils/watch.types'
+
+export function watchObject<T extends object> (obj: T): T & WatchedObject<T> {
+  const callbacks: Map<string | symbol, WatcherCallback<T>[]> = new Map()
+
+  const newObj = {
+    ...obj,
+    on<K extends WatchedProp<T>> (
+      eventName: `${K}Changed`,
+      cb: WatcherCallback<T>,
+    ) {
+      const key = eventName.replace('Changed', '')
+      if (!callbacks.has(key)) { callbacks.set(key, []) }
+      callbacks.get(key)!.push(cb)
+    },
+  }
+
+  return new Proxy(newObj, {
+    set (target, property, value, receiver) {
+      const key = property
+      const result = Reflect.set(target, property, value, receiver)
+      const array = callbacks.get(key)
+      if (array) {
+        array.forEach(cb => cb(value, receiver[key], receiver, property))
+      }
+      return result
+    },
+  })
+}
---
+++ src/engine/utils/watch.types.ts
@@ -0,0 +1,15 @@
+export type WatchedProp<T> = keyof T & string
+
+export type WatcherCallback<T> = <K extends WatchedProp<T>>(
+  value: T[K],
+  oldValue?: T[K],
+  who?: T,
+  prop?: string | symbol,
+) => void
+
+export type WatchedObject<T> = {
+  on: <K extends WatchedProp<T>>(
+    eventName: `${K}Changed`,
+    cb: WatcherCallback<T>,
+  ) => void
+}
