1326965b9e4dfa8a6a4a3c65f6a98f47ded1c579
thalys <thalys@ppd.gg>
2025-12-12 15:48:46 -0300

    refactor: export game start as a named function instead of a default class instance

--- src/app/game.start.ts
+++ src/app/game.start.ts
@@ -4,15 +4,15 @@ import { ScreenAssetLoader } from '@/screens/loading-screen/ScreenAssetLoader'
 import { ScreenMain } from '@/screens/main/ScreenMain'
 import { OverlayUI } from '@/screens/overlay/UI'
 
-export default new class {
-  public async start () {
 
-    const { navigation } = engine()
+export async function start () {
 
-    await navigation.showScreen(ScreenAssetLoader)
-    await navigation.showScreen(ScreenMain)
-    navigation.setOverlay(OverlayUI)
+  const { navigation } = engine()
 
-    logger.info('Starting 2048 game...')
-  }
-}()
+  await navigation.showScreen(ScreenAssetLoader)
+  await navigation.showScreen(ScreenMain)
+  navigation.setOverlay(OverlayUI)
+
+  logger.info('Starting 2048 game...')
+}
+
--- src/app/game.ts
+++ src/app/game.ts
@@ -1,12 +1,11 @@
 import { createApplication } from '@/game.create'
-import game from '@/game.start'
+import { start } from '@/game.start'
 import '@/extra-modules'
 
 async function init () {
 
   await createApplication()
-
-  await game.start()
+  await start()
 }
 
 init()
