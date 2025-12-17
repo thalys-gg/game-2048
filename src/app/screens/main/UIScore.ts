27521e157146b9bb8a8d58dba1b661cc54711815
thalys <thalys@ppd.gg>
2025-12-17 16:30:08 -0300

    feat: Enhance score display with title and value labels, update Label component, and refine game won and reset logic

--- src/app/screens/main/UIScore.ts
+++ src/app/screens/main/UIScore.ts
@@ -16,47 +16,64 @@ function createBackground (parent: Container) {
   const bg = Sprite.from(textures.scoreBg)
   bg.label = 'ScoreBackground'
   bg.alpha = 1
-  bg.anchor.set(0.5)
   parent.addChild(bg)
   return bg
 }
 
-function createLabel (parent: Container, label?: string) {
-  const text = new Label({
-    text: '0',
+function createLabelTitle (parent: Container, text?: string, label?: string) {
+  const _text = new Label({
+    text: text || 'Score',
+    style: {
+      fontSize: 16,
+      fill: theme.score.text,
+      align: 'center',
+    },
+  })
+  _text.label = label || 'ScoreText'
+  _text.alpha = 1
+  _text.anchor.set(0.5)
+  parent.addChild(_text)
+  return _text
+}
+
+function createLabelValue (parent: Container, text?: string, label?: string) {
+  const _text = new Label({
+    text: text || '0',
     style: {
       fontSize: 28,
       fill: theme.score.text,
       align: 'center',
     },
   })
-  text.label = label || 'ScoreText'
-  text.alpha = 1
-  text.anchor.set(0.5)
-  parent.addChild(text)
-  return text
+  _text.label = label || 'ScoreText'
+  _text.alpha = 1
+  parent.addChild(_text)
+  return _text
 }
 
 export class UIScore extends Container implements IChild {
 
   private bg: Sprite
-  private text: Label
+  private textTitle: Label
+  private textValue: Label
 
   private _rollupScoreRef: ReturnType<typeof animations.rollupScore> | null = null
   private _fadeScoreRef: ReturnType<typeof animations.fadeScore> | null = null
 
   constructor () {
     super()
+    this.pivot.set(0.5)
     this.bg = createBackground(this)
-    this.text = createLabel(this)
+    this.textTitle = createLabelTitle(this, 'SCORE')
+    this.textValue = createLabelValue(this, '0')
 
     STATE.on('scoreChanged', (value, oldValue, receiver, property) => {
 
       if (value === 0) {
-        this._fadeScoreRef = animations.fadeScore(this.text)
+        this._fadeScoreRef = animations.fadeScore(this.textValue)
         return
       }
-      this._rollupScoreRef = animations.rollupScore(this.text, oldValue || 0, value)
+      this._rollupScoreRef = animations.rollupScore(this.textValue, oldValue || 0, value)
     })
   }
 
@@ -68,17 +85,20 @@ export class UIScore extends Container implements IChild {
     this._fadeScoreRef = null
   }
 
-  public resize ({ screen }: ResizeSignature) {
+  public resize ({ screen, parent }: ResizeSignature) {
 
-    const center = {
-      x: screen.width * 0.5,
-      y: screen.height * 0.5,
-    }
+    this.x = screen.width * 0.5 - this.bg.width * this.pivot.x
+    this.y = 45
 
-    this.bg.x = center.x
-    this.bg.y = screen.height * 0.1
+    this.bg.x = 0
+    this.bg.y = 0
 
-    this.text.x = center.x
-    this.text.y = this.bg.y
+    const { top, bottom, left, width } = this.bg.getLocalBounds()
+
+    this.textTitle.x = left + width * 0.5
+    this.textTitle.y = top + this.textTitle.height * 0.5 + 10
+
+    this.textValue.x = left + width * 0.5
+    this.textValue.y = bottom - this.textValue.height * 0.5 - 10
   }
 }
